70bf71c31c4c51b7f2184f25800970b45f7c869d
peteruithoven <peter@peteruithoven.nl>
2015-08-07 13:29:03 +0200

    first

---
+++ app.js
@@ -0,0 +1,10 @@
+// importing three.js is not really needed because
+// three.js is a dependency of the STLLoader
+// But to prevent confusion I would always import it anyway in the root js file
+import 'three.js';
+
+console.log('THREE: ', THREE);
+
+import 'three.js/loaders/STLLoader';
+
+console.log('THREE.STLLoader: ', THREE.STLLoader);
