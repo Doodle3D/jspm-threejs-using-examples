30cd82f1f972b61e47f1e4a592c9648c1af2c1d8
peteruithoven <peter@peteruithoven.nl>
2015-08-07 14:00:03 +0200

    Log normalised path

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+
+System.normalize('three.js/loaders/STLLoader')
+  .then(function (path) {
+    console.log(path);
+  });
+
 // importing three.js is not really needed because
 // three.js is a dependency of the STLLoader
 // But to prevent confusion I would always import it anyway in the root js file
-import 'three.js';
-
-console.log('THREE: ', THREE);
-
-import 'three.js/loaders/STLLoader';
-
-console.log('THREE.STLLoader: ', THREE.STLLoader);
+// import 'three.js';
+// console.log('THREE: ', THREE);
+//
+// import 'three.js/loaders/STLLoader';
+// console.log('THREE.STLLoader: ', THREE.STLLoader);
