4485c022af4ada0a1afd72152e48e546087810f2
peteruithoven <peter@peteruithoven.nl>
2015-08-15 17:48:37 +0200

    Using systemjs-debugger

--- config.js
+++ config.js
@@ -11,49 +11,39 @@ System.config({
     "github:*": "jspm_packages/github/*",
     "npm:*": "jspm_packages/npm/*"
   },
-  map: {
-    "babel": "npm:babel-core@5.8.20",
-    "babel-runtime": "npm:babel-runtime@5.8.20",
-    "core-js": "npm:core-js@0.9.18",
-    "three": "github:mrdoob/three.js@master",
-    //"three/loaders/STLLoader": "github:mrdoob/three.js@master/examples/js/loaders/STLLoader.js",
-    "github:jspm/nodelibs-process@0.1.1": {
-      "process": "npm:process@0.10.1"
-    },
-    "npm:babel-runtime@5.8.20": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
-    },
-    "npm:core-js@0.9.18": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.1"
-    }
-  },
+
   packages: {
     "three": {
       "map": {
         "./loaders/STLLoader": "./examples/js/loaders/STLLoader.js"
       },
-      meta: {
+      "meta": {
         "examples/js/loaders/STLLoader.js": {
           "globals": {
             "THREE": "three"
           },
           "exports": "THREE.STLLoader",
           "format": "global"
-        },
+        }
       }
     }
   },
-  meta: {
-    "three/loaders/STLLoader": {
-      "deps": [
-        "three"
-      ],
-      "globals": {
-        "THREE": "three"
-      },
-      "exports": "THREE.STLLoader",
-      "format": "global"
+
+  map: {
+    "babel": "npm:babel-core@5.8.22",
+    "babel-runtime": "npm:babel-runtime@5.8.20",
+    "core-js": "npm:core-js@1.0.1",
+    "systemjs-debugger": "github:peteruithoven/systemjs-debugger@0.0.1",
+    "three": "github:mrdoob/three.js@master",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@5.8.20": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@1.0.1": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1"
     }
   }
 });
